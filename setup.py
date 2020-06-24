f71b7ebaaa1bc39cad6705a52112f3c533846b44
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-06-24 14:06:38 +0200

    🔥 Mark Python 3.4 as unsupported
    It's already been dropped from tox and Travis CI test matrixes
    and now it's time to drop it from the metadata since Python 3.4
    is EOL.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     zip_safe=False,
     install_requires=['pytest>=3.1.0'],
     setup_requires=['setuptools_scm'],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
         'Development Status :: 7 - Inactive',
         'Framework :: Pytest',
@@ -35,7 +35,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
