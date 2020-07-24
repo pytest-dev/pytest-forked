b25d3c3b07068c7037a3ad138c368d9d658756da
Florian Bruhin <The-Compiler@users.noreply.github.com>
2020-07-24 10:57:34 +0300

    testing: make the atexit monkeypatch more robust, needed for pytest 6

--- testing/conftest.py
+++ testing/conftest.py
@@ -9,9 +9,12 @@ def _divert_atexit(request, monkeypatch):
     import atexit
     atexit_fns = []
 
+    def atexit_register(func, *args, **kwargs):
+        atexit_fns.append(lambda: func(*args, **kwargs))
+
     def finish():
         while atexit_fns:
             atexit_fns.pop()()
 
-    monkeypatch.setattr(atexit, "register", atexit_fns.append)
+    monkeypatch.setattr(atexit, "register", atexit_register)
     request.addfinalizer(finish)
